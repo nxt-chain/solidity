246039c1e1e3d5131392782e22da96e480b08bf3
arn-ob <arnobxtreme@gmail.com>
2023-06-04 10:27:22 +0600

    init sol file

--- basic-intro/basic.sol
+++ basic-intro/basic.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0.;
+pragma solidity ^0.8.0;
 
 /// @title This is a  contract
 /// @author arn-ob
 /// @notice 
 /// @dev 
 contract StartContract {
-    
+
 }
