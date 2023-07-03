839aa83c96f019eec71ed4bb2d700744841db58e
arn-ob <arnobxtreme@gmail.com>
2023-07-03 13:39:06 +0600

    update

--- basic-intro/fundMe.sol
+++ basic-intro/fundMe.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.6.6 <0.9.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
+
 
     mapping(address => uint256) public addressToAmountFounded;
 
@@ -9,5 +12,15 @@ contract FundMe {
         addressToAmountFounded[msg.sender] += msg.value;
     }
 
-    
+    function getVersion() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43);
+        return priceFeed.version();
+    }
+
+    function getPrice() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43);
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+        return uint256(answer);
+    }
+
 }
